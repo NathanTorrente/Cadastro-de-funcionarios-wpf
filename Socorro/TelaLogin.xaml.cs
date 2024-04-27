fb8a05e121acc386d9fa87ccd2798d581e625850
Nathan Torrente <nathan.torrente.rocha@gmail.com>
2024-04-27 19:14:12 -0400

    4º Alteração

---
+++ Socorro/TelaLogin.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Socorro
+{
+    /// <summary>
+    /// Lógica interna para TelaLogin.xaml
+    /// </summary>
+    public partial class TelaLogin : Window
+    {
+        public TelaLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow inicio = new MainWindow();
+            inicio.ShowDialog();
+        }
+    }
+}
