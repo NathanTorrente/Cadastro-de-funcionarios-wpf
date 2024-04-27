8ec04021565ce229569c7466008addef63aa910f
Nathan Torrente <nathan.torrente.rocha@gmail.com>
2024-04-27 18:18:52 -0400

    3º Alteração

--- Socorro/MainWindow.xaml.cs
+++ Socorro/MainWindow.xaml.cs
@@ -14,35 +14,62 @@ namespace Socorro
         {
             InitializeComponent();
             TelaMenuEscola = new TelaMenuEscola();
-            TelaMenuEscola.Left = 274; // Adjust position to fit the left grid width
+            TelaMenuEscola.Left = 274; 
             TelaMenuEscola.Top = 0;
-           
-            
            
         }
        
-        private void AbrirTelaMenuEscola()
-        {
-            TelaMenuEscola telaMenuInicio = new TelaMenuEscola();
-           telaMenuInicio.ShowDialog();
-        }
+      
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
 
-            AbrirTelaMenuEscola();
+            
         }
       
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-            
-        }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            TelaMenuEscola telaMenuInicio = new TelaMenuEscola();
+            telaMenuInicio.ShowDialog();
+        }
 
+       
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Aluno(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
